e5dd96d002842f2d4903453fc78b218f25479e78
hq01u8735 <zhangry@metersbonwe.com>
2017-06-27 11:28:44 +0800

    修改数据库为sqlite

--- mock_web/src/main/java/com/tony/test/controller/BootStartServer.java
+++ mock_web/src/main/java/com/tony/test/controller/BootStartServer.java
@@ -1,13 +1,19 @@
 package com.tony.test.controller;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.sqlite.util.StringUtils;
 
 import com.tony.test.protocol.MockServer;
 
@@ -17,6 +23,8 @@ import com.tony.test.protocol.MockServer;
 
     AtomicBoolean                                  isStarted = new AtomicBoolean(false);
 
+    @Resource  BasicDataSource mockDataSource;
+    
     public void start() {
         if (!isStarted.get()) {
             server.start();
@@ -37,6 +45,20 @@ import com.tony.test.protocol.MockServer;
 
     @Override
     public void afterPropertiesSet() throws Exception {
+     	Connection conn =  mockDataSource.getConnection();
+    	Statement stmt =  conn.createStatement() ;
+    	
+    	List<String> aa = IOUtils.readLines(getClass().getClassLoader().getResourceAsStream("sqlite.sql"));
+    	String s = StringUtils.join(aa,"\r\n");
+    	String[] sqls  = s.split(";");
+    	
+    	for (int i = 0; i < sqls.length; i++) {
+			String sql = sqls[i];
+			System.out.println("初始化sql : " + sql);
+	    	stmt.execute(sql);
+		}
+    	stmt.close();
+    	conn.close();
         start();
     }
 
